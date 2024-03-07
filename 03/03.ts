fb015c528d5ab3f17eb46baf4455de53bd4171b0
Evokoo <evanjohncooper@gmail.com>
2024-03-08 02:34:32 +1100

    Day 03-1 complete

--- 03/03.ts
+++ 03/03.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		grid = parseInput(data),
+		trees = checkPath(grid, { x: 3, y: 1 });
+
+	return trees;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +18,28 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "03");
 
 // Functions
+type XY = { x: number; y: number };
+
+function parseInput(data: string) {
+	return data.split("\r\n");
+}
+function checkPath(grid: string[], slope: XY) {
+	const height = grid.length;
+	const width = grid[0].length;
+	const path: XY = { x: 0, y: 0 };
+
+	let trees = 0;
+
+	while (true) {
+		path.y += slope.y;
+		path.x += slope.x;
+
+		if (path.y < height) {
+			if (grid[path.y][path.x % width] === "#") trees++;
+		} else {
+			break;
+		}
+	}
+
+	return trees;
+}
