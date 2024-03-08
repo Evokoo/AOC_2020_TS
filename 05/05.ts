97382c98cc03f59b898391b4bf1c94419a718b2d
Evokoo <evanjohncooper@gmail.com>
2024-03-09 02:14:13 +1100

    Day 05-1 complete

--- 05/05.ts
+++ 05/05.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		boardingPasses = parseInput(data),
+		maxSeatID = locateSeats(boardingPasses);
+
+	return maxSeatID;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +18,43 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "05");
 
 // Functions
+type Seat = { x: number; y: number; ID: number };
+
+function parseInput(data: string) {
+	return data.split("\r\n");
+}
+function locateSeats(passes: string[]) {
+	const seats: Seat[] = [];
+
+	let maxID = 0;
+
+	for (let pass of passes) {
+		let xRange = Array.from({ length: 8 }, (_, i) => i);
+		let yRange = Array.from({ length: 128 }, (_, i) => i);
+
+		for (let char of [...pass]) {
+			switch (char) {
+				case "F":
+					yRange = yRange.slice(0, yRange.length / 2);
+					break;
+				case "B":
+					yRange = yRange.slice(yRange.length / 2);
+					break;
+				case "L":
+					xRange = xRange.slice(0, xRange.length / 2);
+					break;
+				case "R":
+					xRange = xRange.slice(xRange.length / 2);
+					break;
+			}
+		}
+
+		const [x, y] = [xRange[0], yRange[0]];
+		const seat = { x, y, ID: y * 8 + x };
+
+		maxID = Math.max(maxID, seat.ID);
+		seats.push(seat);
+	}
+
+	return maxID;
+}
