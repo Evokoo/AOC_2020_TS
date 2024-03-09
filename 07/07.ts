1a6df6103f59418e5db0bb0a1018fb24aba941de
Evokoo <evanjohncooper@gmail.com>
2024-03-10 02:32:24 +1100

    Day 07-1 complete

--- 07/07.ts
+++ 07/07.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		bags = parseInput(data),
+		validBags = findBags(bags, "shiny gold");
+
+	return validBags.size;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +18,54 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "07");
 
 // Functions
+type Bag = { style: string; quantity: number };
+
+function parseInput(data: string) {
+	const bags: Map<string, Bag[]> = new Map();
+
+	for (let rule of data.split("\r\n")) {
+		const allBags = rule.match(/\w+ \w+(?= bags*)/g) || [];
+		const bagQuantities = rule.match(/\d+/g) || [];
+
+		const style = allBags.shift()!;
+		const contains = allBags
+			.filter((bag) => !bag.startsWith("no"))
+			.map((bag, i) => ({
+				style: bag,
+				quantity: +bagQuantities[i],
+			}));
+
+		bags.set(style, contains);
+	}
+
+	return bags;
+}
+function findBags(bags: Map<string, Bag[]>, target: string) {
+	const valid: Set<string> = new Set();
+
+	for (let [style, innerBags] of bags) {
+		if (style === target) continue;
+
+		const queue = innerBags;
+
+		while (queue.length) {
+			const current = queue.pop()!;
+
+			if (current.style === target || valid.has(current.style)) {
+				valid.add(style);
+				continue;
+			}
+
+			for (const inner of bags.get(current.style) ?? []) {
+				if (inner.style === target) {
+					valid.add(style);
+					continue;
+				} else {
+					queue.push(inner);
+				}
+			}
+		}
+	}
+
+	return valid;
+}
