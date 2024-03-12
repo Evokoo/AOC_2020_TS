476908fc99ee0e054a571c78def53def577dca22
Evokoo <evanjohncooper@gmail.com>
2024-03-13 02:48:32 +1100

    Day 13-1 complete

--- 13/13.ts
+++ 13/13.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		schedule = parseInput(data),
+		result = findBus(schedule);
+
+	return result;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +18,30 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "13");
 
 // Functions
+type Schedule = { target: number; buses: number[] };
+
+function parseInput(data: string): Schedule {
+	const details = data.split("\r\n");
+	const target = +details[0];
+	const buses = details[1]
+		.split(",")
+		.filter((el) => el !== "x")
+		.map(Number);
+
+	return { target, buses };
+}
+function findBus(schedule: Schedule) {
+	const optimalBus = { ID: 0, wait: Infinity };
+
+	for (let bus of schedule.buses) {
+		const depature = Math.ceil(schedule.target / bus) * bus;
+		const wait = depature - schedule.target;
+
+		if (wait < optimalBus.wait) {
+			optimalBus.ID = bus;
+			optimalBus.wait = wait;
+		}
+	}
+
+	return optimalBus.ID * optimalBus.wait;
+}
