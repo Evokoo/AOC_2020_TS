749d156256836c649940c9aeb6b94fd0c0f7d335
Evokoo <evanjohncooper@gmail.com>
2024-03-14 01:06:26 +1100

    Day 14-1 complete

--- 14/14.ts
+++ 14/14.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		updates = parseInput(data),
+		sum = runUpdates(updates);
+
+	return sum;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +18,47 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "14");
 
 // Functions
+type Update = [number, string];
+
+function parseInput(data: string) {
+	const updates: Update[] = [];
+
+	let mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+	for (const line of data.split("\r\n")) {
+		if (line.startsWith("mask")) {
+			mask = line.slice(7);
+		} else {
+			const [address, value] = (line.match(/\d+/g) || []).map(Number);
+
+			let bit = [...value.toString(2).padStart(36, "0")];
+
+			for (let i = 0; i < bit.length; i++) {
+				const maskValue = mask[i];
+
+				if (maskValue !== "X") {
+					bit[i] = maskValue;
+				}
+			}
+
+			updates.push([address, bit.join("")]);
+		}
+	}
+
+	return updates;
+}
+function runUpdates(updates: Update[]) {
+	const memory: Map<number, string> = new Map();
+
+	for (let [address, value] of updates) {
+		memory.set(address, value);
+	}
+
+	let sum = 0;
+
+	for (let [_, value] of memory) {
+		sum += parseInt(value, 2);
+	}
+
+	return sum;
+}
