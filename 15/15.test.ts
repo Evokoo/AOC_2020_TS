a177dc29d35e528db0478337cb7d17731283109e
Evokoo <evanjohncooper@gmail.com>
2024-03-15 01:07:02 +1100

    Day 15 complete

--- 15/15.test.ts
+++ 15/15.test.ts
@@ -7,21 +7,21 @@ const currentDay = path.basename(__dirname);
 describe(`AOC 2020 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		test("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(null);
+			expect(solveA("example_a", currentDay)).toBe(1836);
 		});
 
 		test("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(null);
+			expect(solveA("input", currentDay)).toBe(240);
 		});
 	});
 
 	describe("Part B", () => {
 		test("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(null);
+			expect(solveB("example_b", currentDay)).toBe(362);
 		});
 
 		test("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(null);
+			expect(solveB("input", currentDay)).toBe(505);
 		});
 	});
 });
--- 15/15.ts
+++ 15/15.ts
@@ -3,15 +3,54 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		numbers = parseInput(data),
+		number = playGame(numbers, 2020);
+
+	return number;
 }
 export function solveB(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		numbers = parseInput(data),
+		number = playGame(numbers, 30000000);
+
+	return number;
 }
 
 //Run
-solveA("example_a", "15");
+// solveB("example_b", "15");
 
 // Functions
+function parseInput(data: string) {
+	return data.split(",").map(Number);
+}
+function playGame(numbers: number[], target: number) {
+	const record: Map<number, number[]> = new Map();
+
+	for (let turn = 1, last = 0; true; turn++) {
+		let number = 0;
+
+		if (numbers.length) {
+			number = numbers.shift()!;
+		} else {
+			const history = record.get(last)!;
+
+			if (history.length === 1) {
+				number = 0;
+			} else {
+				const [a, b] = history;
+				number = b - a;
+			}
+		}
+
+		record.set(number, [...(record.get(number) ?? []).slice(-1), turn]);
+		last = number;
+
+		if (turn === target) {
+			console.log(number);
+			return number;
+		}
+	}
+
+	throw Error("Something went wrong");
+}
