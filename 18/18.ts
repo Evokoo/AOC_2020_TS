febe96f8806af96c6ae0f374ccfc7366e1417794
Evokoo <evanjohncooper@gmail.com>
2024-03-18 01:01:46 +1100

    Day 18-1 complete

--- 18/18.ts
+++ 18/18.ts
@@ -3,8 +3,14 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		equations = parseInput(data),
+		sum = equations.reduce(
+			(total, equation) => total + +solveEquation(equation),
+			0
+		);
+
+	return sum;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +21,32 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "18");
 
 // Functions
+function parseInput(data: string) {
+	return data.split("\r\n");
+}
+function solver(a: number, op: string, b: number): string {
+	switch (op) {
+		case "+":
+			return String(a + b);
+		case "*":
+			return String(a * b);
+		default:
+			throw Error("Invalid operator");
+	}
+}
+function solveEquation(equation: string) {
+	const basic = /(\d+) ([+*]) (\d+)/;
+	const braces = /\([\d\s+*]+\)/;
+
+	if (braces.test(equation)) {
+		const eq = equation.replace(/\(([\d\s+*]+)\)/, (_, $) => {
+			return solveEquation($);
+		});
+		return solveEquation(eq);
+	} else if (basic.test(equation)) {
+		const eq = equation.replace(basic, (_, a, op, b) => solver(+a, op, +b));
+		return solveEquation(eq);
+	}
+
+	return equation;
+}
