d3c2b1860ad45e9308fafd1fe8c1b95e98b11f13
Evokoo <evanjohncooper@gmail.com>
2024-03-20 02:26:08 +1100

    Day 21-1 complete

--- 21/21.ts
+++ 21/21.ts
@@ -3,8 +3,12 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		{ foodByAllergen, allFoods } = parseInput(data),
+		dangerous = identifyDangerous(foodByAllergen),
+		safe = identifySafe(dangerous, allFoods);
+
+	return safe.length;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +19,53 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "21");
 
 // Functions
+type FoodByAllergen = Record<string, string[][]>;
+type AllergenMap = Map<string, string>;
+
+function parseInput(data: string) {
+	const foodByAllergen: FoodByAllergen = {};
+	const allFoods: string[][] = [];
+
+	for (const listing of data.split("\r\n")) {
+		const [ingridents, allergens] = listing.split(" (contains");
+		const ingridentList = ingridents.split(" ");
+
+		allFoods.push(ingridentList);
+
+		for (const key of allergens.match(/\w+/g) || []) {
+			foodByAllergen[key] = [...(foodByAllergen[key] ?? []), ingridentList];
+		}
+	}
+
+	return { foodByAllergen, allFoods };
+}
+function identifyDangerous(foodByAllergen: FoodByAllergen) {
+	const identified: Map<string, string> = new Map();
+
+	for (let allergen of Object.keys(foodByAllergen)) {
+		const ingridentLists = foodByAllergen[allergen];
+		const toCheck = ingridentLists.slice(1);
+
+		for (let word of ingridentLists[0]) {
+			if (identified.has(word)) continue;
+
+			if (toCheck.every((list) => list.includes(word))) {
+				identified.set(word, allergen);
+			}
+		}
+	}
+
+	return identified;
+}
+function identifySafe(allergens: AllergenMap, allFoods: string[][]) {
+	const safe: string[] = [];
+
+	for (const food of allFoods) {
+		for (const ingrident of food) {
+			if (allergens.has(ingrident)) continue;
+			else safe.push(ingrident);
+		}
+	}
+
+	return safe;
+}
