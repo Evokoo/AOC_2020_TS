a531aaba1de7c8233537b7d81f21a0a04f5fbc6f
Evokoo <evanjohncooper@gmail.com>
2024-03-22 02:07:13 +1100

    Day 23-1 complete

--- 23/23.ts
+++ 23/23.ts
@@ -1,10 +1,14 @@
 // Imports
 import TOOLS from "../00/tools";
+import { LinkedList, ListNode } from "./LinkedList";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		{ cups, min, max } = parseInput(data),
+		labels = playGame(cups, min, max);
+
+	return +labels;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +19,44 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "23");
 
 // Functions
+function parseInput(data: string) {
+	const digits = [...data].map(Number);
+	const cups = new LinkedList<number>();
+	const min = Math.min(...digits);
+	const max = Math.max(...digits);
+
+	for (let digit of digits) {
+		cups.add(digit);
+	}
+
+	return { cups, min, max };
+}
+
+function playGame(cups: LinkedList<number>, min: number, max: number) {
+	let currentCup: ListNode<number> = cups.head!;
+
+	for (let move = 0; move < 100; move++) {
+		const removed = cups.remove(currentCup.next!, 3);
+		const remaining = new Set(cups.printList);
+
+		let destination = currentCup.data - 1;
+
+		while (!remaining.has(destination)) {
+			destination--;
+
+			if (destination < min) {
+				destination = max;
+			}
+		}
+		cups.insert(destination, removed);
+		currentCup = currentCup.next!;
+	}
+
+	const finalOrder = cups.printList;
+	const oneIndex = finalOrder.indexOf(1);
+
+	return [
+		...finalOrder.slice(oneIndex + 1),
+		...finalOrder.slice(0, oneIndex),
+	].join("");
+}
---
+++ 23/LinkedList.ts
@@ -0,0 +1,152 @@
+export class ListNode<T> {
+	data: T;
+	next: ListNode<T> | null;
+
+	constructor(data: T) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+export class LinkedList<T> {
+	head: ListNode<T> | null;
+	size: number;
+
+	constructor() {
+		this.head = null;
+		this.size = 0;
+	}
+
+	public add = (data: T): void => {
+		const newNode = new ListNode(data);
+
+		if (!this.head) {
+			this.head = newNode;
+			newNode.next = this.head;
+		} else {
+			let current = this.head;
+
+			while (current.next !== this.head) {
+				current = current.next!;
+			}
+
+			current!.next = newNode;
+			newNode.next = this.head;
+		}
+
+		this.size++;
+	};
+
+	public remove(current: ListNode<T>, count: number): T[] {
+		const removedValues: T[] = [];
+		let removedCount = 0;
+		let prev: ListNode<T> | null = null;
+
+		// If the list is empty or count is 0 or negative, return empty array
+		if (!this.head || count <= 0) {
+			return removedValues;
+		}
+
+		// Find the node prior to the current node
+		let temp = this.head;
+		while (temp.next !== current) {
+			temp = temp.next!;
+		}
+		prev = temp;
+
+		// Remove count number of nodes starting from current
+		while (current && removedCount < count) {
+			// Store the next node before removing the current one
+			const nextNode = current.next!;
+
+			// If prev is null and current is the head, update the head to the next node
+			if (!prev && current === this.head) {
+				this.head = nextNode;
+			} else if (prev) {
+				prev.next = nextNode;
+			}
+
+			// Add the data of the current node to removedValues
+			removedValues.push(current.data);
+
+			// If current is the head, update the head to the next node
+			if (current === this.head) {
+				this.head = nextNode;
+			}
+
+			// Move to the next node
+			current = nextNode;
+			removedCount++;
+			this.size--;
+		}
+
+		return removedValues;
+	}
+
+	public insert = (target: T, values: T[]): void => {
+		let current = this.head;
+		let prev: ListNode<T> | null = null;
+
+		// Traverse the list until either the target value is found or the end of the list is reached
+		while (current && current.data !== target) {
+			prev = current;
+			current = current.next;
+			// If current is null, it means we reached the end of the list
+			// and the target value was not found, so we break out of the loop
+			if (!current) {
+				break;
+			}
+		}
+
+		// If current is null, the target value was not found in the list
+		// In this case, we insert the new nodes at the end of the list
+		if (!current) {
+			if (!prev) {
+				// If prev is null, it means the list is empty
+				for (const value of values) {
+					const newNode = new ListNode(value);
+					newNode.next = newNode; // Point to itself for circular behavior
+					this.head = newNode;
+					prev = newNode;
+					this.size++;
+				}
+			} else {
+				for (const value of values) {
+					const newNode = new ListNode(value);
+					newNode.next = this.head; // Point to the head for circular behavior
+					prev.next = newNode;
+					prev = newNode;
+					this.size++;
+				}
+			}
+		} else {
+			// If current is not null, the target value was found in the list
+			for (const value of values) {
+				const newNode = new ListNode(value);
+				newNode.next = current.next; // Insert after the current node
+				current.next = newNode;
+				current = newNode; // Move current to the newly inserted node
+				if (current === this.head) {
+					this.head = newNode; // Update the head if current is the head
+				}
+				this.size++;
+			}
+		}
+	};
+
+	get printList(): T[] {
+		const list: T[] = [];
+
+		if (!this.head) {
+			console.log("List is empty");
+			return [];
+		}
+		let current = this.head;
+		do {
+			list.push(current.data);
+			current = current.next!;
+		} while (current !== this.head);
+
+		return list;
+	}
+}
