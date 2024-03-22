60fa93c9822814789d2622fc8ecf9acd3ab458df
Evokoo <evanjohncooper@gmail.com>
2024-03-23 01:02:11 +1100

    Day 24-1 complete

--- 24/24.ts
+++ 24/24.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		directions = parseInput(data),
+		tileCount = runDirections(directions);
+
+	return tileCount.black;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +18,62 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "24");
 
 // Functions
+type Directions = string[][];
+type Hexagon = { q: number; r: number };
+
+function parseInput(data: string) {
+	const directions: Directions = [];
+
+	for (const line of data.split("\r\n")) {
+		const moves = line.match(/se|sw|ne|nw|e|w/g) || [];
+		directions.push(moves);
+	}
+
+	return directions;
+}
+
+function runDirections(allDirections: Directions) {
+	const tileState: Map<string, boolean> = new Map();
+
+	for (const directions of allDirections) {
+		const position: Hexagon = { q: 0, r: 0 };
+
+		for (const move of directions) {
+			switch (move) {
+				case "e":
+					position.q += 1;
+					break;
+				case "w":
+					position.q -= 1;
+					break;
+				case "ne":
+					position.q += 1;
+					position.r -= 1;
+					break;
+				case "nw":
+					position.r -= 1;
+					break;
+				case "se":
+					position.r += 1;
+					break;
+				case "sw":
+					position.q -= 1;
+					position.r += 1;
+					break;
+				default:
+					throw Error("Invalid direction");
+			}
+		}
+
+		const key = `${position.q},${position.r}`;
+		tileState.set(key, tileState.has(key) ? !tileState.get(key) : false);
+	}
+
+	const tileCount = { black: 0, white: 0 };
+
+	for (const [_, value] of tileState) {
+		value ? tileCount.white++ : tileCount.black++;
+	}
+
+	return tileCount;
+}
