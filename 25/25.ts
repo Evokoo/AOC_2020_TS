2d492a6d57db470db3d6cfd74b3f77b43b8fbe68
Evokoo <evanjohncooper@gmail.com>
2024-03-24 01:30:29 +1100

    Day 25-1 complete

--- 25/25.ts
+++ 25/25.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		{ card, door } = parseInput(data),
+		encryptionKey = getEncryptionKey(card, getLoopSize(door));
+
+	return encryptionKey;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -12,6 +15,37 @@ export function solveB(fileName: string, day: string): number {
 }
 
 //Run
-solveA("example_a", "25");
+solveA("input", "25");
 
 // Functions
+function parseInput(data: string) {
+	const [card, door] = data.split("\r\n").map(Number);
+	return { card, door };
+}
+function processNumber(
+	value: number,
+	subject: number = 7,
+	divisor: number = 20201227
+) {
+	return (value * subject) % divisor;
+}
+function getLoopSize(publicKey: number) {
+	let loopSize = 0;
+	let value = 1;
+
+	while (value !== publicKey) {
+		value = processNumber(value);
+		loopSize++;
+	}
+
+	return loopSize;
+}
+function getEncryptionKey(publicKey: number, loops: number) {
+	let key = 1;
+
+	for (let i = 0; i < loops; i++) {
+		key = processNumber(key, publicKey);
+	}
+
+	return key;
+}
